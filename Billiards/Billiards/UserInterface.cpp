b3ecdf59d0dfe915e0572158de658d5606589210
lapp14 <d.lapp14@gmail.com>
2013-12-22 23:23:11 -0500

    Fixed table and ball models
    Ball model is centered on (0, 0, 0), table bed is centered on (0, 0,
    -ballRadius). Will remodel cue for next commit and am working on getting
    UI going.

---
+++ Billiards/Billiards/UserInterface.cpp
@@ -0,0 +1,27 @@
+
+
+#include "UserInterface.h"
+
+
+
+/**
+ * glut keyboard function.
+ *
+ * @param key the character struck
+ * @param x the x coord of where the mouse was when key was struck
+ * @param y the y coord of where the mouse was when key was struck
+
+void keyboardFunc(unsigned char key, int x, int y){
+    switch (key){
+    case ESC:
+        break;
+    }
+}
+**/
+
+UserInterface::UserInterface(){
+
+	//GLUI_Master.set_glutKeyboardFunc(keyboardFunc);
+
+
+};
---
+++ Billiards/Billiards/UserInterface.h
@@ -0,0 +1,19 @@
+
+
+
+#include "GL\glui.h"
+
+#define ESC 27
+
+class UserInterface{
+
+public:
+	UserInterface();
+	//void keyboardFunc(unsigned char key, int x, int y);
+
+protected:
+	GLUI* glui;
+
+};
+
+
